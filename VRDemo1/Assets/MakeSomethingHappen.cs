02be028b390ab2c6f0a3d33c0370e4b0592ed042
astarks11 <astarks@email.arizona.edu>
2017-07-21 15:14:34 -0700

    7/21/201

---
+++ VRDemo1/Assets/MakeSomethingHappen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeSomethingHappen : MonoBehaviour {
+
+    public GameObject door;
+
+    // Use this for initialization
+    void Start () {
+        GameObject.FindObjectOfType<CapsuleCollider>().isTrigger = true;
+        }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // works if istrigger is marked
+        door.GetComponent<Move>().activateGate();
+
+    }
+
+
+}
---
+++ VRDemo1/Assets/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void activateGate()
+    {
+
+        transform.SetPositionAndRotation(new Vector3(this.transform.position.x, this.transform.position.y - 1, this.transform.position.z),this.transform.rotation);
+    }
+}
